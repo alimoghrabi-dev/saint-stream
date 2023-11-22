c070f03be799f40ac06246e1179d95d73e1ec06d
i-JavaScript-Master-i <amoghrabi168@gmail.com>
2023-11-22 09:36:41 +0200

    setting webhook 3

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/webhook"],
+  ignoredRoutes: ["/api/webhook"],
 });
 
 export const config = {
